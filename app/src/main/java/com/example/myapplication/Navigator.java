b261cb54e2c43be1b8131b705806d9f966ea8f0c
thomasbud <thomas.budihardjo@gmail.com>
2022-03-10 09:10:25 -0600

    added navigation feature

--- app/src/main/java/com/example/myapplication/Navigator.java
+++ app/src/main/java/com/example/myapplication/Navigator.java
@@ -2,7 +2,19 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Navigator extends AppCompatActivity {
 
@@ -10,5 +22,224 @@ public class Navigator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator);
+
+        //background img
+        ImageView storeLayout = (ImageView) findViewById(R.id.storeLayout);
+        //int imageResource = getResources().getIdentifier("@drawable/layout", null, this.getPackageName());
+        //storeLayout.setImageResource(imageResource);
+
+        //bitmap, using the above ImageView
+        Bitmap bitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        storeLayout.setImageBitmap(bitmap);
+
+        @SuppressLint("UseCompatLoadingForDrawables") Drawable d = getResources().getDrawable(R.drawable.layout, null);
+        d.setBounds(0,0,1080,1920);
+        d.draw(canvas);
+
+        // Line
+        Paint paint = new Paint();
+        paint.setColor(Color.rgb(255, 153, 51));
+        int strokeWidth = 16;
+        paint.setStrokeWidth(strokeWidth);
+        paint.setAntiAlias(true);
+        Stack<int[]> pathStack = generatePathStack(7);
+        while(!pathStack.isEmpty()) {
+            int[] c = pathStack.pop();
+            //yes, I had to switch the x and the y
+            int leftOffset = 12;
+            int topOffset = 35;
+            int startX = c[1]*26 + leftOffset;
+            int startY = c[0]*26 + topOffset;
+            int endX = c[1]*26 + leftOffset + strokeWidth;
+            int endY = c[0]*26 + topOffset;
+
+            Log.d("drawing", c[0] + " " + c[1]);
+            canvas.drawLine(startX, startY, endX, endY, paint);
+        }
+    }
+    private Stack<int[]> generatePathStack(int aisle) {
+        Stack<int[]> res = new Stack<>();
+
+        //grid: 0 invalid path (obstructions), 1 valid path, 2 starting point (entrance), 3-9 destinations
+        int[][] grid = {
+                { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,8,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1 },
+                { 1,0,0,0,7,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,6,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1 },
+                { 1,1,1,5,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,3,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,4,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1 },
+                { 1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+                { 0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0 } };
+
+        QItem source = new QItem(0, 0, 0);
+        //locate start
+        firstLoop:
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[i].length; j++) {
+                if(grid[i][j] == 2) {
+                    source.row = i;
+                    source.col = j;
+                    break firstLoop;
+                }
+            }
+        }
+
+        //bfs
+        Queue<QItem> queue = new LinkedList<>();
+        queue.add(new QItem(source.row, source.col, 0));
+
+        int[][] distance = new int[grid.length][grid[0].length];
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        visited[source.row][source.col] = true;
+
+        QItem destination = new QItem(0,0,0);
+        while(!queue.isEmpty()) {
+            QItem p = queue.remove();
+
+            //if destination found
+            if(grid[p.row][p.col] == aisle) {
+                destination.row = p.row;
+                destination.col = p.col;
+                destination.dist = distance[p.row][p.col];
+                break;
+            }
+
+            //check up
+            if (isValid(p.row - 1, p.col, grid, visited)) {
+                queue.add(new QItem(p.row - 1, p.col, p.dist + 1));
+                distance[p.row - 1][p.col] = p.dist + 1;
+                visited[p.row - 1][p.col] = true;
+            }
+            //check left
+            if (isValid(p.row, p.col - 1, grid, visited)) {
+                queue.add(new QItem(p.row, p.col - 1, p.dist + 1));
+                distance[p.row][p.col - 1] = p.dist + 1;
+                visited[p.row][p.col - 1] = true;
+            }
+            //check down
+            if (isValid(p.row + 1, p.col, grid, visited)) {
+                queue.add(new QItem(p.row + 1, p.col, p.dist + 1));
+                distance[p.row + 1][p.col] = p.dist + 1;
+                visited[p.row + 1][p.col] = true;
+            }
+            //check right
+            if (isValid(p.row, p.col + 1, grid, visited)) {
+                queue.add(new QItem(p.row, p.col + 1, p.dist + 1));
+                distance[p.row][p.col + 1] = p.dist + 1;
+                visited[p.row][p.col + 1] = true;
+            }
+        }
+
+        //backtracking
+        visited = new boolean[grid.length][grid[0].length]; //reset visited matrix; we don't need this rly
+        int distFromStart = destination.dist;
+        int row = destination.row;
+        int col = destination.col;
+        while(distFromStart > 0) {
+            distFromStart--;
+            //check up
+            if (isValid(row - 1, col, grid, visited)) {
+                if(distance[row - 1][col] == distFromStart) {
+                    res.push(new int[]{--row, col});
+                    continue;
+                }
+            }
+            //check left
+            if (isValid(row, col - 1, grid, visited)) {
+                if(distance[row][col - 1] == distFromStart) {
+                    res.push(new int[]{row, --col});
+                    continue;
+                }
+            }
+            //check down
+            if (isValid(row + 1, col, grid, visited)) {
+                if(distance[row + 1][col] == distFromStart) {
+                    res.push(new int[]{++row, col});
+                    continue;
+                }
+            }
+            //check right
+            if (isValid(row, col + 1, grid, visited)) {
+                if(distance[row][col + 1] == distFromStart) {
+                    res.push(new int[]{row, ++col});
+                    //continue;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private static boolean isValid(int x, int y, int[][] grid, boolean[][] visited) {
+        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] != 0 && !visited[x][y];
+    }
+}
+
+class QItem {
+    int row;
+    int col;
+    int dist;
+    public QItem(int row, int col, int dist) {
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
     }
 }
